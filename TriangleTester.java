f940b53555a4746fec7cff39f9b7302cbf88170c
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-29 16:06:51 -0400

    Tested getArea method.

--- TriangleTester.java
+++ TriangleTester.java
@@ -2,5 +2,6 @@ public class TriangleTester{
   public static void main(String[]args){
   Triangle t1 = new Triangle(0,0,3,2,5,0);
   System.out.println(t1.getPerimeter());
+  System.out.println(t1.getArea());
   }
 }
