c246f5cadff8d914ab648cc3779632739a9c8413
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-29 15:38:32 -0400

    Tested distanceTo method.

--- PointTester.java
+++ PointTester.java
@@ -1,10 +1,15 @@
 public class PointTester{
   public static void main(String[]args){
-    Point p1 = new Point(2,2);
+    Point p1 = new Point(0,0);
     System.out.println(p1.getX());
     System.out.println(p1.getY());
     Point p2 = new Point(p1);
     System.out.println(p2.getX());
     System.out.println(p2.getY());
+    Point p3 = new Point(3,0);
+    Point p4 = new Point(7,5);
+    System.out.println(p1.distanceTo(p2));
+    System.out.println(p1.distanceTo(p3));
+    System.out.println(p1.distanceTo(p4));
   }
 }
