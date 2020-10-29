9b9439040160024a2d9ce64a0f156883dc0a3f0f
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-29 16:11:19 -0400

    Tested classify method.

--- TriangleTester.java
+++ TriangleTester.java
@@ -3,5 +3,10 @@ public class TriangleTester{
   Triangle t1 = new Triangle(0,0,3,2,5,0);
   System.out.println(t1.getPerimeter());
   System.out.println(t1.getArea());
+  Triangle t2 = new Triangle(0,0,1,0,0,1);
+  Triangle t3 = new Triangle(0,0,6,0,3,Math.sqrt(27));
+  System.out.println(t1.classify());
+  System.out.println(t2.classify());
+  System.out.println(t3.classify());
   }
 }
