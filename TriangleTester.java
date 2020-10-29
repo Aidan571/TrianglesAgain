5011bd65fefefd4f73f8f2931e370f510ad90f8f
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-29 16:29:02 -0400

    Updated test cases for setVertex

--- TriangleTester.java
+++ TriangleTester.java
@@ -5,13 +5,17 @@ public class TriangleTester{
   System.out.println(t1.getArea());
   Triangle t2 = new Triangle(0,0,1,0,0,1);
   Triangle t3 = new Triangle(0,0,6,0,3,Math.sqrt(27));
+  Point p1 = new Point(2,3);
   System.out.println(t1.classify());
   System.out.println(t2.classify());
   System.out.println(t3.classify());
   System.out.println(t1.toString());
+  t1.setVertex(1,p1);
+  System.out.println(t1.toString());
+  System.out.println(t2.toString());
+  t2.setVertex(0,p1);
   System.out.println(t2.toString());
   System.out.println(t3.toString());
-  Point p1 = new Point(2,3);
   t3.setVertex(2,p1);
   System.out.println(t3.toString());
   }
